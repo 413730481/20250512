a5747aa9395036d552763ad6c70f4c940b4f471c
413730481 <a0912665202@gmail.com>
2025-05-12 10:35:27 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 let facemesh;
 let video;
 let predictions = [];
-const points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
+// 嘴巴的外部輪廓索引
+const points = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 61];
 
 function setup() {
   createCanvas(400, 400);
@@ -29,17 +30,15 @@ function draw() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
 
+    // 繪製嘴巴的外部輪廓
     for (let i = 0; i < points.length - 1; i++) {
       const indexA = points[i];
       const indexB = points[i + 1];
-      const [xA, yA] = keypoints[indexA];
-      const [xB, yB] = keypoints[indexB];
-      line(xA, yA, xB, yB); // 繪製兩點之間的線
+      if (keypoints[indexA] && keypoints[indexB]) {
+        const [xA, yA] = keypoints[indexA];
+        const [xB, yB] = keypoints[indexB];
+        line(xA, yA, xB, yB); // 繪製兩點之間的線
+      }
     }
-
-    // 將最後一點與第一點連接，形成閉合圖形
-    const [xStart, yStart] = keypoints[points[0]];
-    const [xEnd, yEnd] = keypoints[points[points.length - 1]];
-    line(xEnd, yEnd, xStart, yStart);
   }
 }
